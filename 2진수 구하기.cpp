854da61d30a805425dcf3a239715be1c13125076
sweetpotatoqueen <62874856+sweetpotatoqueen@users.noreply.github.com>
2020-05-28 12:40:58 +0900

    Add files via upload

---
+++ 2진수 구하기.cpp
@@ -0,0 +1,26 @@
+﻿#pragma warning (disable:4996)
+#include <stdio.h>
+int main()
+{
+	int N,v=1;
+	scanf("%d", &N);
+
+	while (2 * v <= N)
+		v *= 2;
+
+	
+	while (v > 0)
+	{
+		if (N >= v)
+		{
+			N = N - v;
+			printf("1");
+			v /= 2;
+		}
+		else
+		{
+			printf("0");
+			v /= 2;
+		}
+	}
+}
