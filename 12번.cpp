cc6baa8db6c54a4dca64cf57a6fd8919fac84f43
sweetpotatoqueen <62874856+sweetpotatoqueen@users.noreply.github.com>
2020-06-05 02:20:03 +0900

    Add files via upload

---
+++ 12번.cpp
@@ -0,0 +1,14 @@
+﻿#pragma warning (disable:4996)
+#include <stdio.h>
+void get_int(int *px, int *py, int sum)
+{
+	sum= *px + *py;
+	printf("%d", sum);
+}
+int main()
+{
+	int x,y,sum=0;
+	printf("두 개의 정수를 입력하시오:");
+	scanf("%d,%d", &x, &y);
+	get_int(&x, &y,sum);
+}
