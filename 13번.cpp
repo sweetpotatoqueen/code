049ddbef04fe9da4f1abb85930e0b73b32655074
sweetpotatoqueen <62874856+sweetpotatoqueen@users.noreply.github.com>
2020-06-07 23:02:37 +0900

    Add files via upload

---
+++ 13번.cpp
@@ -0,0 +1,17 @@
+﻿#include <stdio.h>
+void digit(int n)
+{
+	int i;
+	int binary[32] = { 0 };
+	for (i = 0; i < 32 && n>0; i++)
+	{
+		binary[i] = n % 2;
+		n = n / 2;
+	}
+	for (i--; i >= 0; i--)
+		printf("%d", binary[i]);
+}
+int main()
+{
+	digit(32);
+}
