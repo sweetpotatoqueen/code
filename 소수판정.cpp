be8d4f5acb12ee77312be216e82aca896ed49122
sweetpotatoqueen <62874856+sweetpotatoqueen@users.noreply.github.com>
2020-06-23 19:11:01 +0900

    Add files via upload

---
+++ 소수판정.cpp
@@ -0,0 +1,21 @@
+﻿#pragma warning (disable:4996)
+#include <stdio.h>
+int checkprime(int n)
+{
+	for (int i = 2; i*i < n; i++)	
+		if (n % i == 0)
+			return 0;
+		return 1;
+}
+int main()
+{
+	int a;
+	scanf("%d", &a);
+
+	int result = checkprime(a);
+
+	if (result == 1)
+		printf("prime");
+	else 
+		printf("not prime");
+}
