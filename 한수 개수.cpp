eb95a4cb7e8993de553d7bf00ac5a5a4a8ca6298
sweetpotatoqueen <62874856+sweetpotatoqueen@users.noreply.github.com>
2020-07-07 23:20:35 +0900

    Add files via upload

---
+++ 한수 개수.cpp
@@ -0,0 +1,24 @@
+﻿#pragma warning (disable:4996)
+#include <stdio.h>
+int main()
+{
+	int n, count = 0, x, y, z;
+	scanf("%d", &n);
+
+	if (n < 100)
+		printf("%d", n);
+
+	else
+	{
+		for (int i = 101; i <= n; i++)
+		{
+			x = i / 100;
+			y = (i / 10) % 10;
+			z = i % 10;
+
+			if ((x - y) == (y - z))
+				count++;
+		}
+		printf("%d", count + 99);
+	}
+}
