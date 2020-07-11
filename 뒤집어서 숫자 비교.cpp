8052bbd343d1229d689d5f0d945fd9fc4705f8e5
sweetpotatoqueen <62874856+sweetpotatoqueen@users.noreply.github.com>
2020-07-11 11:50:14 +0900

    Add files via upload

---
+++ 뒤집어서 숫자 비교.cpp
@@ -0,0 +1,28 @@
+﻿#pragma warning (disable:4996)
+#include <stdio.h>
+int main()
+{
+	int n, m, data1[3] = { 0 }, data2[3] = { 0 }, i = 0, j=0;
+	scanf("%d %d", &n, &m);
+
+	while (n != 0)
+	{
+		data1[i] = n % 10;
+		n /= 10;
+		i++;
+	}
+	while (m != 0)
+	{
+		data2[j] = m % 10;
+		m /= 10;
+		j++;
+	}
+
+	n = (data1[0] * 10 + data1[1]) * 10 + data1[2];
+	m = (data2[0] * 10 + data2[1]) * 10 + data2[2];
+
+	if (m > n)
+		printf("%d", m);
+	else
+		printf("%d", n);
+}
