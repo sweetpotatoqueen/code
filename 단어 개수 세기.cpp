160e3e575a73efaece519993778d2316dbf49afb
sweetpotatoqueen <62874856+sweetpotatoqueen@users.noreply.github.com>
2020-07-13 23:40:25 +0900

    Add files via upload

---
+++ 단어 개수 세기.cpp
@@ -0,0 +1,27 @@
+﻿#include <stdio.h>
+#include <ctype.h>
+int countword(char* s)
+{
+	int count = 0, waiting = 1;
+	for(int i=0; s[i]!=NULL; ++i)
+		if (isalpha(s[i]))
+		{
+			if (waiting)
+			{
+				count++;
+				waiting = 0;
+			}
+		}
+	else
+		waiting = 1;
+
+	return count;
+}
+int main()
+{
+	char buffer[100];
+	gets_s(buffer, 100);
+	int result = countword(buffer);
+	printf("%d", result);
+}
+
