9af2e0714b1d3c279199b553c26f37566f9056b4
sweetpotatoqueen <62874856+sweetpotatoqueen@users.noreply.github.com>
2020-07-14 22:24:38 +0900

    Add files via upload

---
+++ 알파벳 나오는 위치.cpp
@@ -0,0 +1,24 @@
+﻿#pragma warning (disable:4996)
+#include <stdio.h>
+#include <string.h>
+int main()
+{
+	char data[101] = { NULL }, count[101] = { NULL };
+	scanf("%s", data);
+
+	for (int i = 0; i < 26; i++)
+		count[i] = -1;
+
+	for(int i='a'; i<='z'; i++)
+		for (int j = 0; j < strlen(data); j++)
+		{
+			if (data[j] == i)
+			{
+				count[i - 97] = j;
+				break;
+			}
+		}
+
+	for (int i = 0; i < 26; i++)
+		printf("%d ", count[i]);
+}
