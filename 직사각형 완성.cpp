3207d623114c41e1b4187651390125a19fa0537c
sweetpotatoqueen <62874856+sweetpotatoqueen@users.noreply.github.com>
2020-07-17 00:19:05 +0900

    Add files via upload

---
+++ 직사각형 완성.cpp
@@ -0,0 +1,20 @@
+﻿#pragma warning (disable:4996)
+#include <stdio.h>
+int main()
+{
+	int a, b, c, d, e, f, i = 0;
+	scanf("%d %d %d %d %d %d", &a, &b, &c, &d, &e, &f);
+
+	if (a == c)
+		printf("%d ", e);
+	else if (a == e)
+		printf("%d ", c);
+	else if (c == e)
+		printf("%d ", a);
+	if (b == d)
+		printf("%d", f);
+	else if (b == f)
+		printf("%d", d);
+	else if (d == f)
+		printf("%d", b);
+}
