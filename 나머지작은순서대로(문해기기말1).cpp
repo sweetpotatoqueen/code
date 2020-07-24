eea68ee68c7a968ba767a2c2011ea4e41187a96f
sweetpotatoqueen <62874856+sweetpotatoqueen@users.noreply.github.com>
2020-07-24 23:19:00 +0900

    Add files via upload

---
+++ 나머지작은순서대로(문해기기말1).cpp
@@ -0,0 +1,45 @@
+﻿#pragma warning (disable:4996)
+#include <stdio.h>
+#define MAX 100
+int main()
+{
+	int N, data[MAX], k, rest[MAX];
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++)
+		scanf("%d", &data[i]);
+
+	scanf("%d", &k);
+
+	for (int i = 0; i < N; i++)
+		rest[i] = data[i] % k;
+
+	for(int i=N-1; i>0; i--)
+		for (int j = 0; j < i; j++)
+		{
+			if (rest[j] > rest[j + 1])
+			{
+				int temp = rest[j];
+				rest[j] = rest[j + 1];
+				rest[j + 1] = temp;
+				int tmp = data[j];
+				data[j] = data[j + 1];
+				data[j + 1] = tmp;
+			}
+			else if (rest[j] == rest[j + 1])
+			{
+				if (data[j] > data[j + 1])
+				{
+					int temp = data[j];
+					data[j] = data[j + 1];
+					data[j + 1] = temp;
+					int tmp = rest[j];
+					rest[j] = rest[j + 1];
+					rest[j + 1] = tmp;
+				}
+			}
+		}
+
+	for (int i = 0; i < N; i++)
+		printf("%d ", data[i]);
+}
