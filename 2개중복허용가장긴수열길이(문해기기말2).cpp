0a6e59128830268397061b7d0ba87f82321cd908
sweetpotatoqueen <62874856+sweetpotatoqueen@users.noreply.github.com>
2020-07-25 19:36:44 +0900

    Add files via upload

---
+++ 2개중복허용가장긴수열길이(문해기기말2).cpp
@@ -0,0 +1,33 @@
+﻿#pragma warning (disable:4996)
+#include <stdio.h>
+#define MAX 100
+int main()
+{
+	int N, data[MAX], array[MAX] = { 0 }, count = 0, max = 0;
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++)
+		scanf("%d", &data[i]);
+
+	for (int i = 0; i < N; i++)
+	{
+		count = 0; 
+        for (int i = 0; i < N; i++)
+			array[i] = { 0 }; //초기화
+
+		for (int j = i; j < N; j++)
+		{
+			array[data[j]]++;
+			if (array[data[j]] >= 3) //3개 이상 나오면 count세기 전에 break
+			{
+				if (count > max)
+					max = count;
+				break;
+			}
+			count++;
+			if (count > max)
+				max = count;
+		}
+	}
+	printf("%d", max);
+}
