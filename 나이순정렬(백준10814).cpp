fb1e18b78d4fe4f4dc5377000b4304675ef0e918
sweetpotatoqueen <62874856+sweetpotatoqueen@users.noreply.github.com>
2020-08-04 18:10:27 +0900

    Add files via upload

---
+++ 나이순정렬(백준10814).cpp
@@ -0,0 +1,28 @@
+﻿#pragma warning (disable:4996)
+#include <stdio.h>
+#include <limits.h>
+#include <malloc.h>
+typedef struct customer {
+	int age;
+	char name[101];
+}customer;
+struct customer c[100001];
+int main()
+{
+	int N, max = INT_MIN, min = INT_MAX;
+	scanf("%d", &N);
+
+	for (int i = 0; i < N; i++)
+	{
+		scanf("%d %s", &c[i].age, c[i].name);
+		if (min > c[i].age)
+			min = c[i].age;
+		if (max < c[i].age)
+			max = c[i].age;
+	}
+
+	for (int i = min; i <=max; i++)
+		for(int j=0; j<N; j++)
+			if(c[j].age==i)
+				printf("%d %s\n", c[j].age, c[j].name);
+}
