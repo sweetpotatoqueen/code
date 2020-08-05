e9bd359e6028d3f385db9fd38ecb74954b49db82
sweetpotatoqueen <62874856+sweetpotatoqueen@users.noreply.github.com>
2020-08-06 00:44:19 +0900

    Add files via upload

---
+++ 연속된정답마다배점추가계산(백준 8958).cpp
@@ -0,0 +1,28 @@
+﻿#pragma warning (disable:4996)
+#include <stdio.h>
+#include <string.h>
+#define MAX 80
+int main()
+{
+	int n, sum, result=0;
+	char data[MAX];
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		sum = 0;
+		result = 1;
+		scanf("%s", data);
+		for (int j = 0; j < strlen(data); j++)
+		{
+			if (data[j] == 'O')
+			{
+				sum += result;
+				result++;
+			}
+			else if (data[j] == 'X')
+				result = 1;
+		}
+		printf("%d\n", sum);
+	}
+}
