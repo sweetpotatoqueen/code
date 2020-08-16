0683b274bbf6fee206eb769da879a5dcb9deacc3
sweetpotatoqueen <62874856+sweetpotatoqueen@users.noreply.github.com>
2020-08-16 23:46:57 +0900

    Add files via upload

---
+++ 문자열 반복(백준 2675).cpp
@@ -0,0 +1,23 @@
+﻿#pragma warning (disable:4996)
+#include <stdio.h>
+#include <string.h>
+#define MAX 20
+void print(char a[], int b)
+{
+	for (int i = 0; i < strlen(a); i++)
+		for (int j = 0; j < b; j++)
+			printf("%c", a[i]);
+	printf("\n");
+}
+int main()
+{
+	int n, j;
+	char data[MAX];
+	scanf("%d", &n);
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &j);
+		scanf("%s", data);
+		print(data, j);
+	}
+}
