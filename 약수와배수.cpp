8888bb7a48c75b735f4c55f31dcdc835f9e0ca9f
sweetpotatoqueen <62874856+sweetpotatoqueen@users.noreply.github.com>
2020-08-26 21:56:23 +0900

    Add files via upload

---
+++ 약수와배수.cpp
@@ -0,0 +1,28 @@
+﻿#pragma warning (disable:4996)
+#include <stdio.h>
+int main()
+{
+	int data1, data2;
+
+	while (1)
+	{
+		scanf("%d %d", &data1, &data2);
+	
+		if (data1 == 0 && data2 == 0)
+			break;
+		if (data1 < data2)
+		{
+			if (data2 % data1 == 0)
+				printf("factor\n");
+			else
+				printf("neither\n");
+		}
+		else if (data1 > data2)
+		{
+			if (data1 % data2 == 0)
+				printf("multiple\n");
+			else
+				printf("neither\n");
+		}
+	}
+}
