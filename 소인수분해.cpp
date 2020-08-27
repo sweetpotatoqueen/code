e56554e57cd4996d11c87d098737d9fdfad210a5
sweetpotatoqueen <62874856+sweetpotatoqueen@users.noreply.github.com>
2020-08-27 19:36:48 +0900

    Add files via upload

---
+++ 소인수분해.cpp
@@ -0,0 +1,18 @@
+﻿#pragma warning (disable:4996)
+#include <stdio.h>
+int main()
+{
+	int n, i=2;
+	scanf("%d", &n);
+
+	while (n>1)
+	{
+		if (n % i == 0)
+		{
+			printf("%d\n", i);
+			n /= i;
+		}
+		else
+			i++;
+	}
+}
