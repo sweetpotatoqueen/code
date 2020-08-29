4c1c05296d2da1673c3a0df86043fcc075daf042
sweetpotatoqueen <62874856+sweetpotatoqueen@users.noreply.github.com>
2020-08-29 20:59:09 +0900

    Add files via upload

---
+++ 이항계수.cpp
@@ -0,0 +1,19 @@
+﻿#pragma warning (disable:4996)
+#include <stdio.h>
+int factorial(int n)
+{
+	int result = 1;
+	for (int i = 1; i <= n; i++)
+		result *= i;
+
+	return result;
+}
+int main()
+{
+	int n, k;
+	scanf("%d %d", &n, &k);
+
+	int result = factorial(n) / (factorial(k) * factorial(n - k));
+
+	printf("%d", result);
+}
