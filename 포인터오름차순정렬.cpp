3c95528f75c2eebca473822321b2773664464bef
sweetpotatoqueen <62874856+sweetpotatoqueen@users.noreply.github.com>
2020-09-05 15:20:12 +0900

    Add files via upload

--- 포인터오름차순정렬.cpp
+++ 포인터오름차순정렬.cpp
@@ -1,31 +1,30 @@
 ﻿#pragma warning (disable:4996)
 #include <stdio.h>
 #include <stdlib.h>
-void sort(int * p)
+void sort(int* p , int N)
 {
-	for (int i = 0; i <= sizeof(p)-1; i++) {
-		for (int j = i+1; j <= sizeof(p); j++)
-			if ( *(p + i) > *(p + j))
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N-i-1 ; j++)
+			if (*(p + j) > * (p + j + 1))
 			{
-				int temp = *(p + i);
-				*(p + i) = *(p + j);
-				*(p + j) = temp;
+				int temp = *(p + j);
+				*(p + j) = *(p + j + 1);
+				*(p + j + 1) = temp;
 			}
 	}
-	return;
 }
 int main()
 {
-	int N, count = 0;
+	int N;
 	scanf("%d", &N);
 
-	int * n = (int*)malloc(sizeof(int) * N);
+	int *n = (int *)malloc(sizeof(int) * N);
 
 	for (int i = 0; i < N; i++)
 		scanf("%d", (n + i));
 
-	sort(n);
+	sort(n, N);
 
-	for (int i = 0; i < N; i++) 
+	for (int i = 0; i < N; i++)
 		printf("%d\n", *(n + i));
 }
